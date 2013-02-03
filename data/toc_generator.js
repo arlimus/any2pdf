cc6f2cbbfacc660b16b80eb5a4d7f046382aa48a
Dominik Richter <dominik.richter@gmail.com>
2013-02-03 15:11:54 +0100

    added css styling for toc
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- data/toc_generator.js
+++ data/toc_generator.js
@@ -28,7 +28,7 @@ $(function () {
 
     // Open parent lists if a child list follows.
     while (assigned_level < current_level) {
-      toc_html += "<ol>";
+      toc_html += "<ol class=\"toc\">";
       assigned_level += 1;
     }
 
